ee29bcca45b9432dc6b7fe81cc8973f75859ae34
Rukattsu <rukattsu@gmail.com>
2023-10-04 17:44:53 -0300

    continuação do login de acesso

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class LoginForm extends JDialog {
     private JTextField tfEmail;
@@ -16,10 +19,81 @@ public class LoginForm extends JDialog {
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
+                user = getAuthenticatedUser(email,password);
+
+                if(user != null){
+                    dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(LoginForm.this,
+                            "Email ou Password invalido",
+                            "Tentar novamente",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
         setVisible(true);
+    }
+    public User user;
+    private User getAuthenticatedUser(String email, String password){
+        User user = null;
+
+        final String DB_URL = "jbdc:postgresql://localhost";
+        final String USERNAME = "lucas";
+        final String PASSWORD = "2221";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                user = new User();
+                user.name = resultSet.getString("name");
+                user.email = resultSet.getString("email");
+                user.phone = resultSet.getString("phone");
+                user.address = resultSet.getString("address");
+                user.password = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public static void main(String[] args) {
         LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+
+        if (user != null){
+            System.out.println(" Sucesso na autenticação para : " + user.name);
+            System.out.println("           Email : " + user.name);
+            System.out.println("           Phone : " + user.name);
+            System.out.println("           Address : " + user.name);
+        }else {
+            System.out.println("Autenticação cancelada");
+        }
     }
 }
