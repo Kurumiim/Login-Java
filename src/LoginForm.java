f31cd8fe2cdaecf432d42ee7f17df45987a0e67d
unknown <Administrador@MICRO101-LB.ADM.LOCAL>
2023-10-04 16:13:18 -0300

    tela de login feita em java

---
+++ src/LoginForm.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends JDialog {
+    private JTextField tfEmail;
+    private JPasswordField pfPassword;
+    private JButton btnOK;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+
+    public LoginForm(JFrame parent){
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm(null);
+    }
+}
